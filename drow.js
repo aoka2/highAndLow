75d4c41ed1e7297378680d961f4547588bcee3f4
aoka2 <aoka543222@gmail.com>
2024-09-09 14:50:09 +0900

    ドロー機能修正
    画像が画面に出るようになりました。
    画面内のHTMLにはidがroom0からroom3に設定されているimgタグが必要です。

--- drow.js
+++ drow.js
@@ -5,7 +5,12 @@ async function drow(roomObject,deckID){
             const card = await card_data.json();
             roomObject[i] = card; 
         }
+        const card_img = document.getElementById(`room${i}`);
+        
+        const imageUrl = await roomObject[i].cards[0]["image"]; 
+
+        card_img.src = card_img.src = imageUrl;
     }
-    
+
     return roomObject;
 }
